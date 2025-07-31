076ca2dceb95a5bd38d0ea609a25de7dc1a88280
SravyaPola <sravyapola2000@gmail.com>
2025-07-31 14:12:17 -0700

    Modified shell script

--- scripts/gen-inventory.sh
+++ scripts/gen-inventory.sh
@@ -2,6 +2,6 @@
 IP=$(terraform -chdir=terraform output -raw nifi_public_ip)
 cat > inventory.ini <<INI
 [ec2]
-${IP} ansible_user=ubuntu ansible_ssh_private_key_file=/home/ubuntu/.ssh/nifi-key
+${IP} ansible_user=ubuntu ansible_ssh_private_key_file=/home/ubuntu/.ssh/nifi-key ansible_ssh_common_args='-o StrictHostKeyChecking=no'
 INI
 echo "Wrote inventory.ini → ${IP}"
