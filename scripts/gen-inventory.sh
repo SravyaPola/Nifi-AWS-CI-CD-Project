16cfb1edccc7b3b9e5f7d70a400a9df150ba9a20
SravyaPola <sravyapola2000@gmail.com>
2025-08-02 20:09:03 -0700

    Modified yaml files and Jenkinsfile

--- scripts/gen-inventory.sh
+++ scripts/gen-inventory.sh
@@ -7,7 +7,9 @@ IP=$(terraform -chdir=terraform output -raw nifi_public_ip)
 # write a clean inventory
 cat > inventory.ini <<EOF
 [ec2]
-$IP ansible_user=ubuntu ansible_ssh_common_args='-o StrictHostKeyChecking=no'
+$IP ansible_user=ubuntu \
+    ansible_ssh_common_args='-o StrictHostKeyChecking=no -o ForwardAgent=yes'
 EOF
+
 
 echo "Wrote inventory.ini → $IP"
