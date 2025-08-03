09d9256da56cf23a6a3740d2de0d2f09ac03dae2
SravyaPola <sravyapola2000@gmail.com>
2025-08-02 22:45:53 -0700

    Modified yaml files and Jenkinsfile

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "nifi_public_ip" {
-  description = "Public IP of the NiFi EC2"
-  value       = aws_instance.nifi.public_ip
-}
-
-output "artifact_bucket" {
-  description = "S3 bucket for NiFi ZIP"
-  value       = aws_s3_bucket.nifi_artifacts.bucket
-}
